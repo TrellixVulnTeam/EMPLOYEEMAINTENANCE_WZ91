391fa6439b032bfa1f9239a39d9156c45023311c
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-07-27 14:02:36 -0400

    Add obtenerEdificios method, Add bootstrap template for edificios, display edificios data

--- UI/ANGULAR/src/app/edificios/ver-borrar/ver-borrar.component.ts
+++ UI/ANGULAR/src/app/edificios/ver-borrar/ver-borrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ManagmentService } from 'src/app/services/managment.service';
 
 @Component({
   selector: 'app-ver-borrar',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerBorrarComponent implements OnInit {
 
-  constructor() { }
+  edificios: any = [];
+
+  constructor(private managementService: ManagmentService) { }
 
   ngOnInit(): void {
+    this.asignarDatosEdificios();
   }
 
+  asignarDatosEdificios(){
+      this.managementService.obtenerEdificios()
+          .subscribe(edificios =>{
+            console.log(edificios)
+             this.edificios = edificios
+          })
+  }
 }
